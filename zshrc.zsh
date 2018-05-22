73e2ee38825a576358f4ca1196908af8958b1581
Ethan Breder <ebreder@apple.com>
2018-05-22 10:40:05 -0700

    more edits

--- zshrc.zsh
+++ zshrc.zsh
@@ -2,10 +2,12 @@ setopt EXTENDED_GLOB
 ZDOTDIR=~/dot source ~/dot/.zprezto/init.zsh
 export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/code
+
 # Doesn't work on non osx
-#export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
+# export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
+# source /usr/local/bin/virtualenvwrapper.sh
+
 export EDITOR="emacs"
-source /usr/local/bin/virtualenvwrapper.sh
 alias emacs="emacs -nw"
 alias dc="docker-compose"
 
