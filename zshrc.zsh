69f8aac4da166e4e33a56acd37be7031d58fb3d8
Ethan Breder <ebreder@apple.com>
2018-05-22 10:14:03 -0700

    powerlevel changes

--- zshrc.zsh
+++ zshrc.zsh
@@ -5,8 +5,14 @@ export PROJECT_HOME=$HOME/code
 export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
 source /usr/local/bin/virtualenvwrapper.sh
 alias emacs="emacs -nw"
+alias dc="docker-compose"
 
 # kubectl completion is really slow to initialize
 #if [ $commands[kubectl] ]; then
 #  source <(kubectl completion zsh)
 #fi
+
+### powerlevel9k
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir)
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator virtualenv)
+DEFAULT_USER=$USER
