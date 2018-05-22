de177db8bcd13ada1887df6442eed50831a191b3
Ethan Breder <ebreder@apple.com>
2018-05-22 10:21:42 -0700

    editor

--- zshrc.zsh
+++ zshrc.zsh
@@ -3,9 +3,11 @@ ZDOTDIR=~/dot source ~/dot/.zprezto/init.zsh
 export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/code
 export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
+export EDITOR="emacs"
 source /usr/local/bin/virtualenvwrapper.sh
 alias emacs="emacs -nw"
 alias dc="docker-compose"
+
 
 # kubectl completion is really slow to initialize
 #if [ $commands[kubectl] ]; then
